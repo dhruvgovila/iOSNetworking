58b86a55ff7b6173dcf88123ed2dc0bd1846ba4b
Govila, Dhruv (US - Bengaluru) <dgovila@deloitte.com>
2020-03-04 20:01:36 +0530

    Setting up project

---
+++ iOSNetworking/iOSNetworking/Source/Netwoking.swift
@@ -0,0 +1,26 @@
+//
+//  Netwoking.swift
+//  iOSNetworking
+//
+//  Created by Govila, Dhruv on 04/03/20.
+//  Copyright © 2020 Govila, Dhruv (US - Bengaluru). All rights reserved.
+//
+
+import Foundation
+let session = URLSession.shared
+
+open class Networking {
+    
+    public static let shared: Networking = Networking()
+    
+    open func makeApiCall(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let url = URL(string: "https://learnappmaking.com/ex/users.json")!
+        let task = session.dataTask(with: url, completionHandler: { data, response, error in
+            completionHandler(data, response, error)
+        })
+        task.resume()
+    }
+}
+
+
+
---
+++ iOSNetworking/iOSNetworking/iOSNetworking.h
@@ -0,0 +1,19 @@
+//
+//  iOSNetworking.h
+//  iOSNetworking
+//
+//  Created by Govila, Dhruv on 04/03/20.
+//  Copyright © 2020 Govila, Dhruv (US - Bengaluru). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSNetworking.
+FOUNDATION_EXPORT double iOSNetworkingVersionNumber;
+
+//! Project version string for iOSNetworking.
+FOUNDATION_EXPORT const unsigned char iOSNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSNetworking/PublicHeader.h>
+
+
